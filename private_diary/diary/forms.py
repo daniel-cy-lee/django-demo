2170360f8652317caa15706aaf9ac694ce017f31
Daniel <danielcylee@outlook.com>
2024-09-04 11:05:20 +0800

    修正複製貼上錯誤
    51, 52, 54 頁

--- private_diary/diary/forms.py
+++ private_diary/diary/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
 class InquiryForm(forms.Form):
-    name = forms.CharField(label="姓名", max_length=30) 
+    name = forms.CharField(label="姓名", max_length=30)
     email = forms.EmailField(label="信箱")
     title = forms.CharField(label="主旨", max_length=50)
     message = forms.CharField(label="內容", widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
 
         self.fields["name"].widget.attrs["class"] = "form_control"
         self.fields["name"].widget.attrs["placeholder"] = "請輸入您的姓名"
@@ -21,3 +21,6 @@ class InquiryForm(forms.Form):
 
         self.fields["message"].widget.attrs["class"] = "form_control"
         self.fields["message"].widget.attrs["placeholder"] = "請輸入信件內容"
+
+    def send_email(self):
+        name = self.cleaned_data["name"]
